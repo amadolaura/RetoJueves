61c7b4040bd48cf2bb986adfdb2fb32e3fe1344e
Your Name <you@example.com>
2021-10-29 09:19:00 -0500

    first commit

---
+++ js/Mensajes.js
@@ -0,0 +1,161 @@
+function consultar() {
+    $.ajax({
+        url: "http://129.151.106.231:8080/api/Message/all",
+        type: "GET",
+        dataType: "JSON",
+        //success - propiedad
+        //function(json) - funcion anonima
+        success: function(respuesta) {
+            // capa div - resultado
+            $("#resultado").empty();
+            for (i = 0; i < respuesta.length; i++) {
+                $("#resultado").append("<tr>");
+                $("#resultado").append("<td>" + "&nbsp;&nbsp;&nbsp;&nbsp;" + respuesta[i].idMessage + "&nbsp;&nbsp;&nbsp;&nbsp;" + "</td>");
+                $("#resultado").append("<td>" + "&nbsp;&nbsp;&nbsp;&nbsp;" + respuesta[i].messageText + "&nbsp;&nbsp;&nbsp;&nbsp;" + "</td>");
+                //$("#miResultado").append('<td><input type="button" value="BORRAR" onclick="eliminar(' + json.items[i].id + ')"></td>');
+                $("#resultado").append("</tr>")
+            };
+            console.log(respuesta);
+        },
+        error: function(xhr, status) {
+            alert('Ha sucedido un problema, ' + xhr.status);
+        },
+        complete: function(xhr, status) {
+            //status - es el estado de codigo
+            alert('Petición realizada, ' + xhr.status);
+        }
+    });
+
+}
+
+
+function registrar() {
+
+    let var2 = {
+        messageText:$("#messageText").val()   
+        };
+      
+        $.ajax({
+        type:'POST',
+        contentType: "application/json; charset=utf-8",
+        dataType: 'JSON',
+        data: JSON.stringify(var2),
+        
+        url:"http://129.151.106.231:8080/api/Message/save",
+       
+        
+        success:function(response) {
+                console.log(response);
+            console.log("Se guardo correctamente");
+            alert("Se guardo correctamente");
+            window.location.reload()
+    
+        },
+        
+        error: function(jqXHR, textStatus, errorThrown) {
+              window.location.reload()
+            alert("No se guardo correctamente");
+    
+    
+        }
+        });
+
+}
+
+function editar() {
+    let myData = {
+        idMessage: $("#idMessage").val(),
+        messageText:$("#messageText").val()
+    }
+    let dataTosend = JSON.stringify(myData);
+    $.ajax({
+
+        url: "http://129.151.106.231:8080/api/Message/update",
+        type: "PUT",
+        data: dataTosend,
+        contentType: "application/JSON",
+        dataType: 'JSON',
+        success: function(respuesta) {
+            $("#resultado").empty();
+            $("#idMessage").val("");
+            $("#messageText").val("");
+            consultar();
+        },
+        error: function(xhr, status) {
+            alert('Ha sucedido un problema' + xhr.status);
+        },
+        complete: function(xhr, status) {
+            alert('Petición realizada' + xhr.status);
+            //limpiarFormulario();
+        }
+    });
+}
+
+
+function eliminar(id) {
+    let myData = {
+        id: id.val()
+    }
+    let dataToSend = JSON.stringify(myData)
+    $.ajax({
+        url: "http://129.151.106.231:8080/api/Message/" + id.val(),
+        type: 'DELETE',
+        //data: dataToSend,
+        //contentType: "application/JSON",
+        datatype: "JSON",
+        success: function(repuesta) {
+            $("#resultado").empty();
+            consultar();
+            console.log(json);
+            console.log("idMessage", dataToSend)
+            debugger
+        },
+        error: function(xhr, status) {
+            alert('ha sucedido un problema' + xhr.status);
+        },
+        complete: function(xhr, status) {
+            alert('Petición realizada ' + xhr.status);
+            //limpiarFormulario();
+        }
+    });
+
+}
+
+/**
+function buscarPorID(id) {
+    $.ajax({
+        url: 'http://localhost:8080/api/Message/' + id.val(),
+        dataType: 'json',
+        type: 'GET',
+        success: function(json) {
+            $("#resultado").empty();
+            $("#resultado").append("<tr>");
+            $("#resultado").append("<td>" + "&nbsp;&nbsp;&nbsp;&nbsp;" + json.items[0].idMessage + "&nbsp;&nbsp;&nbsp;&nbsp;" + "</td>");
+            $("#resultado").append("<td>" + "&nbsp;&nbsp;&nbsp;&nbsp;" + json.items[0].messageText + "&nbsp;&nbsp;&nbsp;&nbsp;" + "</td>");
+            $("#resultado").append("</tr>");
+
+            //$("#resultado").append( json.items[0].room+"&nbsp;&nbsp;&nbsp;&nbsp;"
+            //+json.items[0].stars+"&nbsp;&nbsp;&nbsp;&nbsp;"
+            //+json.items[0].category_id+"&nbsp;&nbsp;&nbsp;"
+            //+json.items[0].description);
+            console.log(json);
+            console.log("idMessage", id.val())
+            debugger
+        },
+        error: function(xhr, status) {
+            alert('ha sucedido un problema' + xhr.status);
+        },
+        complete: function(xhr, status) {
+            alert('Petición realizada ' + xhr.status);
+        }
+    });
+}
+
+
+function limpiarFormulario() {
+    $("#messagetext").val("");
+}
+
+function soloLectura() {
+    $("#id").prop("readonly", false);
+}*/
